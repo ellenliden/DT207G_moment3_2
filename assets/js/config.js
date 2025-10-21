eb325aa2970a9f56625ed35f0f8666c7609368b0
Ellen <lidenellen@gmail.com>
2025-10-21 15:11:07 +0200

    Uppdaterat konfiguration - config.js uppdaterad så att min frontend pekar på rätt backend och endpoint.

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
-// API-konfiguration för moment2_2 frontend
+// API-konfiguration för moment3_steg2 frontend
 const API_CONFIG = {
-  baseUrl: "https://dt207g-moment2-1-ph2b.onrender.com",
-  itemsPath: "/api/users", //API-endpoint för användare
+  baseUrl: "https://dt207g-moment3-rf1b.onrender.com",
+  itemsPath: "/api/experiences", //API-endpoint för arbetserfarenheter
 };
